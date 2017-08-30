a469d36412fad1aea5be28829174044af6fffa60
skyline <574048497@qq.com>
2017-08-30 16:45:18 +0800

    优化代码
    优化代码

--- app/src/main/java/com/example/administrator/imageloader/diskcache/cachemanager/BaseCacheManager.java
+++ app/src/main/java/com/example/administrator/imageloader/diskcache/cachemanager/BaseCacheManager.java
@@ -1,50 +1,17 @@
 package com.example.administrator.imageloader.diskcache.cachemanager;
 
+import java.io.Closeable;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.Reader;
-import java.io.Writer;
 
 /**
  * Created by sujizhong on 16/5/16.
  */
 public abstract class BaseCacheManager {
 
-    public final void IOclose(OutputStream outputStream) {
-        if (outputStream != null) {
+    public final void IOclose(Closeable closeable) {
+        if (closeable != null) {
             try {
-                outputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public final void IOclose(InputStream inputStream) {
-        if (inputStream != null) {
-            try {
-                inputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public final void IOclose(Writer writer) {
-        if (writer != null) {
-            try {
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public final void IOclose(Reader reader) {
-        if (reader != null) {
-            try {
-                reader.close();
+                closeable.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
